fffcd24c78bfa60639a5f25acb8e9ad99c625092
Talha Ansari <talhajansari@hotmail.com>
2017-05-26 19:16:50 -0400

    experiemented with bower

--- client/routes.js
+++ client/routes.js
@@ -7,19 +7,41 @@ var router = express.Router();
 var path_to_pages = './client//public/pages'
 var path_to_assets = './client/public/assets'
 
-router.get('/todo', function(req, res) {
-	res.sendfile('./client/public/pages/todo/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+/* =============================================================================
+ ASSETS 
+===============================================================================*/
+
+router.get('/assets/*', function(req, res) {
+	var path = req.params[0]
+	res.contentType(path_to_assets + '/' + path);
+	res.sendfile(path_to_assets + '/' + path); 
 });
 
+router.get('/media/*', function(req, res) {
+
+	console.log(req);
+	
+	var page = req.params.page;
+	var path = req.params[0]
+	res.contentType(path_to_pages + '/' + page + '/' + path);
+	res.sendfile(path_to_pages + '/' + page + '/' + path); 
+});
+
+router.get('/:page/media/*', function(req, res) {
+
+	var page = request.params.page;
+	var path = req.params[0]
+	res.contentType(path_to_pages + '/' + page + '/' + path);
+	res.sendfile(path_to_pages + '/' + page + '/' + path); 
+});
+
+/* =============================================================================
+ ROUTES
+===============================================================================*/
 router.get('/index', function(req, res) {
 	res.sendfile(path_to_pages + '/index/index.html'); 
 });
 
-router.get('/asset/*', function(req, res) {
-	var path = req.params[0]
-	
-	res.contentType(path_to_assets + '/' + path);
-	res.sendfile(path_to_assets + '/' + path); 
-});
+
 
 module.exports = router
